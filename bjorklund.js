1ca042cd972c562d0722598e352eaa98f6e87a8f
abrie <abrie@>
2014-09-14 23:09:51 +0100

    refactor: extract bjorklund function into module

---
+++ bjorklund.js
@@ -0,0 +1,64 @@
+function generate( steps, pulses, pulseFirst ) {
+    var divisor = steps - pulses;
+    var level = 0;
+
+    if( pulses === 0) {
+        var result = [];
+        for( var index = 0; index < steps; index++ ) {
+            result.push(0);
+        }
+        return result;
+    }
+
+    var pattern = [];
+    var counts = [];
+    var remainders = [];
+
+    remainders.push( pulses );
+
+    while( true ) {
+        var count = Math.floor( divisor / remainders[level] );
+        counts.push( count );
+        var remainder = divisor % remainders[level];
+        remainders.push( remainder );
+        divisor = remainders[level];
+        level = level + 1;
+        if( remainders[level] <= 1 ) {
+            break;
+        }
+    }
+
+    counts.push(divisor);
+
+    function build( l ) {
+        if( l === -1 ) {
+            pattern.push( 0 );
+            return;
+        }
+        if( l === -2 ) {
+            pattern.push( 1 );
+            return;
+        }
+
+        for( var i = 0; i < counts[l]; i++ ) {
+            build( l - 1 );
+        }
+
+        if( remainders[l] !== 0 ) {
+            build( l - 2 );
+        }
+    }
+
+    build( level );
+
+    if(pulseFirst) {
+        var first = pattern.indexOf( 1 );
+        var slice = pattern.slice(0, first);
+        pattern.splice(0,first);
+        pattern = pattern.concat(slice);
+    }
+
+    return pattern;
+}
+
+module.exports = generate;
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var Queue = require('./queue.js');
 var midi = require('./midi.js');
+var bjorklund = require('./bjorklund.js');
 
 PPQN = 24;
 
@@ -64,11 +65,11 @@ function Emitter(patterns) {
 
 var emitterA = new Emitter({
     0:[
-        { channel: 1, pattern: generateBjorklund(5,3), stepSize: PPQN/4 },
+        { channel: 1, pattern: bjorklund(5,3), stepSize: PPQN/4 },
     ],
     1:[
-        { channel: 0, pattern: generateBjorklund(12,7), stepSize: PPQN/4 },
-        { channel: 2, pattern: generateBjorklund(24,13,true), stepSize: PPQN },
+        { channel: 0, pattern: bjorklund(12,7), stepSize: PPQN/4 },
+        { channel: 2, pattern: bjorklund(24,13,true), stepSize: PPQN },
     ]
 });
 
@@ -89,65 +90,4 @@ midi.setMessageHandlers({
     onSPP: function() {}});
 
 
-function generateBjorklund( steps, pulses, pulseFirst ) {
-    var divisor = steps - pulses;
-    var level = 0;
 
-    if( pulses === 0) {
-        var result = [];
-        for( var index = 0; index < steps; index++ ) {
-            result.push(0);
-        }
-        return result;
-    }
-
-    var pattern = [];
-    var counts = [];
-    var remainders = [];
-
-    remainders.push( pulses );
-
-    while( true ) {
-        var count = Math.floor( divisor / remainders[level] );
-        counts.push( count );
-        var remainder = divisor % remainders[level];
-        remainders.push( remainder );
-        divisor = remainders[level];
-        level = level + 1;
-        if( remainders[level] <= 1 ) {
-            break;
-        }
-    }
-
-    counts.push(divisor);
-
-    function build( l ) {
-        if( l === -1 ) {
-            pattern.push( 0 );
-            return;
-        }
-        if( l === -2 ) {
-            pattern.push( 1 );
-            return;
-        }
-
-        for( var i = 0; i < counts[l]; i++ ) {
-            build( l - 1 );
-        }
-
-        if( remainders[l] !== 0 ) {
-            build( l - 2 );
-        }
-    }
-
-    build( level );
-
-    if(pulseFirst) {
-        var first = pattern.indexOf( 1 );
-        var slice = pattern.slice(0, first);
-        pattern.splice(0,first);
-        pattern = pattern.concat(slice);
-    }
-
-    return pattern;
-}
